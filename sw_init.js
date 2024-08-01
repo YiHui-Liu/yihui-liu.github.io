264157f99e726560473cf408b58a1088c6e77d31
liuyihui <liuyihui02@gmail.com>
2024-08-02 00:14:39 +0800

    Site updated: 2024-08-02 00:14:38

--- sw_init.js
+++ sw_init.js
@@ -1,1 +1,1 @@
-(async()=>{"serviceWorker"in navigator?(console.log("FOW_SW is uninstalling."),navigator.serviceWorker.getRegistrations().then(function(e){for(let i of e)i.active.scriptURL.includes("foolishfox.cn/sw.js")&&i.unregister()})):console.log("FOW_SW is not supported.")})();
+(async()=>{"serviceWorker"in navigator?(console.log("FOW_SW is uninstalling."),navigator.serviceWorker.getRegistrations().then(function(e){for(let i of e)i.active.scriptURL.includes("/sw.js")&&i.unregister()})):console.log("FOW_SW is not supported.")})();
